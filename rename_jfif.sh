bf5ac87c6e60a1a86d6f127ec8e72014bcc24c7e
Enryu <enryuvgc@gmail.com>
2026-04-29 16:36:24 +0530

    sinnoh added

--- rename_jfif.sh
+++ rename_jfif.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 for f in lunar/*.jfif; do
-  mv "$f" "${f%.jfif}.png"
+  [ -f "$f" ] && mv "$f" "${f%.jfif}.png"
+done
+for f in lunar/*.jpg; do
+  [ -f "$f" ] && mv "$f" "${f%.jpg}.png"
 done
 echo "Done."
